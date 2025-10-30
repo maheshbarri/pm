fb9619c830978ee82d3384e8782beb7122082f52
maheshbarri <buma2072@gmail.com>
2025-10-30 11:10:18 +0530

    first commit

---
+++ functions/f1.sql
@@ -0,0 +1,11 @@
+create or replace function f1(a number)
+	return varchar2
+	is
+	begin
+	if mod(a,2)=0 then
+		return ‘even’;
+	else
+		return ‘odd’;
+	end if;
+	end;
+        /
---
+++ procedures/sp_git.sql
@@ -0,0 +1,15 @@
+create or replace procedure sp_git(pi_deptno  in number)
+is
+cursor c1 is select ename,sal from emp_a  where deptno=pi_deptno;
+v_ename varchar2(10);
+v_sal number;
+begin
+open c1;
+loop
+fetch c1 into v_ename,v_sal;
+exit when c1%notfound;
+dbms_output.put_line(v_ename||' '||v_sal);
+end loop;
+close c1;
+end;
+/
